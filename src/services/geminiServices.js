df6ada63339c1d0df82364eb861c36994d05639b
Ana Paula <ana.campos.btu@gmail.com>
2024-11-22 13:55:26 -0300

    altera-imagens-do-servidor-e-adiciona-pasta-com-capturas-de-tela-do-projeto

--- src/services/geminiServices.js
+++ src/services/geminiServices.js
@@ -5,7 +5,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default async function gerarDescricaoComGemini(imageBuffer) {
   const prompt =
-    "Gere uma descrição em português do brasil para a seguinte imagem";
+    "Descreve a seguinte imagem";
 
   try {
     const image = {
